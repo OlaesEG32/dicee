04ba5895b65f20c5ef47e659ded31c1d3e56bcec
OlaesEG32 <145347124+OlaesEG32@users.noreply.github.com>
2023-11-19 21:31:18 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-var player1 = Math.floor(Math.random() * 6) + 1;
-var player2 = Math.floor(Math.random() * 6) + 1;
+$(document).ready(function() {
+  function diceRoll() {
+      return Math.floor(Math.random() * 6) + 1;
+  }
 
-var DiceImg1 = "images/dice" + player1 + ".png";
-var DiceImg2 = "images/dice" + player2 + ".png";
+  function changeImg(player, diceValue) {
+      $(`.img${player}`).attr('src', `images/dice${diceValue}.png`);
+  }
 
-var imageArray1 = document.querySelectorAll("img")[0].setAttribute("src", DiceImg1);
-var imageArray2 = document.querySelectorAll("img")[1].setAttribute("src", DiceImg2);
+  function winner() {
+      const player1Dice = diceRoll();
+      const player2Dice = diceRoll();
 
-if (player1 > player2) {
-  document.querySelector("h1").innerHTML = "Player 1 Wins !"
-} else if (player1 < player2) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins !"
-} else {
-  document.querySelector("h1").innerHTML = "T I E"
-}
+      changeImg(1, player1Dice);
+      changeImg(2, player2Dice);
+
+      if (player1Dice > player2Dice) {
+          $('h1').text('Player 1 Wins!');
+      } else if (player1Dice < player2Dice) {
+          $('h1').text('Player 2 Wins!');
+      } else {
+          $('h1').text('Draw!');
+      }
+  }
+
+  winner();
+
+});
+
